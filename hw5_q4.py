6a6cc2066ad08301bdbb8112dd38e2b2d20454f7
NSkyeKirsch <73858119+NSkyeKirsch@users.noreply.github.com>
2022-03-10 17:23:04 -0500

    finished q4

--- hw5_q4.py
+++ hw5_q4.py
@@ -7,4 +7,17 @@ collaborating with anyone else, in conformance with the
 NYU School of Engineering Policies and Procedures on
 Academic Misconduct.
 """
+descending = True;
+saved_letter = 100000;
 
+in_string = input("Please enter aa string of lowercase letters: ");
+
+for letter in in_string:
+    if ord(letter) >= saved_letter:
+        descending = False;
+    saved_letter = ord(letter);
+
+if(descending == False):
+    print(in_string, "is not decreasing.");
+else:
+    print(in_string, "is decreasing.");
