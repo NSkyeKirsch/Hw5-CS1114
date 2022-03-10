62d7d6254eab1f84511af36af8b81182ad6f829b
NSkyeKirsch <73858119+NSkyeKirsch@users.noreply.github.com>
2022-03-10 15:01:59 -0500

    finished q2

--- hw5_q2.py
+++ hw5_q2.py
@@ -8,3 +8,20 @@ NYU School of Engineering Policies and Procedures on
 Academic Misconduct.
 """
 
+#without using islower, isupper, lower, upper... Convert to lowercase!
+
+converted_str = "";
+num_changes = 0;
+
+in_string = input("Please Enter a String: ");
+
+for character in in_string:
+    chr_ord = ord(character); #converts to a number
+    if chr_ord >= 65 and chr_ord <= 90: #if within range of capital letters
+        converted_str += chr(chr_ord + 32); #adding 32 makes it lowercase ASCII, and chr() changes it back into a regular character (not a number)
+        num_changes += 1;
+    else:
+        converted_str += chr(chr_ord);
+
+print(converted_str);
+print("Number of changes:", num_changes);
